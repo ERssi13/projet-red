e413d33ac8e351d06e5f866c46ef4942f397919d
ERssi13 <enzo.rossi13@ynov.com>
2024-09-18 16:12:18 +0200

    Create clear.go

---
+++ clear.go
@@ -0,0 +1,19 @@
+package projetred
+
+import (
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+func clearTerminal() {
+	var cmd *exec.Cmd
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("cmd", "/c", "cls")
+	default:
+		cmd = exec.Command("clear")
+	}
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
